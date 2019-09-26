57ec3155dceff709c0c354128fa8918523358efe
Sahar Elsayed <Sahar.Elsayed@ibtikar.com>
2019-09-26 16:30:03 +0200

    first commit: create app, setup sass, setup bootstrap, setup fonts, adding sass partials

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App mt-lg-60">
+      App
+      <p>Hello</p>
+    </div>
+  );
+}
+
+export default App;
